70694c88b86c2355089349e5bd506fa6d6f62096
Kirill <kirillvikhlyaev88@yandex.ru>
2022-03-28 01:06:05 +0500

    Практика над сайтом

---
+++ crm/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .models import Order
+from .forms import OrderForm
+from cms.models import CmsSlider
+from price.models import PriceCard, PriceTable
+
+
+# Create your views here.
+def first_page(request):
+    slider_list = CmsSlider.objects.all()
+    pc_1 = PriceCard.objects.get(pk=5)
+    pc_2 = PriceCard.objects.get(pk=6)
+    pc_3 = PriceCard.objects.get(pk=7)
+    form = OrderForm();
+    price_table = PriceTable.objects.all()
+    dict_obj = {'slider_list': slider_list,
+                'price_table': price_table,
+                'pc_1': pc_1,
+                'pc_2': pc_2,
+                'pc_3': pc_3,
+                'form': form
+                }
+    return render(request, './index.html', dict_obj)
+
+
+def thanks_page(request):
+    name = request.POST['name']
+    phone = request.POST['phone']
+    element = Order(order_name=name, order_phone=phone)
+    element.save()
+    return render(request, './thanks-page.html', {'name': name})
